34a5172115935063948a130f4621ae82c9de7a33
Heni-Nm <anmi65492@gmail.com>
2025-04-22 16:35:03 -0700

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
-
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 mongoose
 .connect(process.env.MONGO)
@@ -15,13 +15,14 @@ mongoose
     
 });
 const app = express();
-
-const USER_ROUTE = '/api/user';
-app.use(USER_ROUTE, userRouter);
-
+app.use(express.json());
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 app.listen(3000, () =>{
     console.log('Server is running on port 3000!');
     
 }
 );
+
+
 
