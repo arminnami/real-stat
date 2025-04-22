0ba099da8a1ea02c3556a3187568e82843b66984
Heni-Nm <anmi65492@gmail.com>
2025-04-22 12:15:36 -0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+
+dotenv.config();
+mongoose
+.connect(process.env.MONGO)
+.then(()=>{
+    console.log('connected to MONGO');
+    
+})
+.catch((err) => {
+    console.error(err);
+    
+});
 const app = express();
 
 app.listen(3000, () =>{
